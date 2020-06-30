9813b288be5471929e8e373e5f3162bc341c56fb
davigdc <davigdc10@gmail.com>
2020-06-30 20:30:39 -0300

    Pesquisar e salvar pesquisa

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ const API_KEY = '03feddc6c6834cacae2825190c984ed2';
 // Primeiro conteudo, noticias do Brasil
 window.onload = executaPesquisa(1);
 
+function readLocalStorage() {
+  let stringData = localStorage.getItem('db');
+  let newLocalStorage = {
+    savedSearchs: [],
+  };
+
+  if (stringData) {
+    return JSON.parse(stringData);
+  } else {
+    saveOnLocalStorage(newLocalStorage);
+    return JSON.parse(localStorage.getItem('db'));
+  }
+}
+
+function saveOnLocalStorage(data) {
+  localStorage.setItem('db', JSON.stringify(data));
+}
+
 // Renderiza noticias no main
 function exibeNoticias() {
   let divTela = document.getElementById('listNews');
@@ -44,6 +62,54 @@ function exibeNoticias() {
   divTela.innerHTML = texto;
 }
 
+function search(savedSearch) {
+  let inputSearch = document.getElementById('textSearch').value;
+  let query = savedSearch ? savedSearch : inputSearch;
+
+  if (query === '') return null;
+
+  let xhr = new XMLHttpRequest();
+  xhr.onload = exibeNoticias;
+  xhr.open(
+    'GET',
+    `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`,
+  );
+  xhr.send();
+
+  event.preventDefault();
+
+  if (!savedSearch) saveSearchs(query);
+}
+
+function saveSearchs(search) {
+  let db = readLocalStorage();
+
+  db.savedSearchs.push(search);
+
+  saveOnLocalStorage(db);
+
+  loadPreviusSearchs();
+}
+
+function loadPreviusSearchs() {
+  let db = readLocalStorage();
+
+  if (!db) return null;
+
+  let ulSearch = document.getElementById('savedSearchs');
+  let textResponse = '';
+
+  if (db.savedSearchs.length === 0) return null;
+
+  db.savedSearchs.forEach(search => {
+    textResponse += `
+      <li onclick="search('${search}')">${search}</li>
+    `;
+  });
+
+  ulSearch.innerHTML = textResponse;
+}
+
 // Retorna dados da API
 function executaPesquisa(prop) {
   let country = prop === 1 ? 'br' : 'us';
@@ -55,6 +121,8 @@ function executaPesquisa(prop) {
     `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`,
   );
   xhr.send();
+
+  loadPreviusSearchs();
 }
 
 // Retorna dados da API
